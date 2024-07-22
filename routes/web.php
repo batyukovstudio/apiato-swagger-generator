42d401add6de65df35bf1551358c36414e92a701
Igor Cherepanov <65646152+igor-cherepanov@users.noreply.github.com>
2024-07-22 15:38:52 +0500

    fix: swagger only for auth:web

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Batyukovstudio\ApiatoSwaggerGenerator\Http\Controllers\SwaggerController;
 
-Route::get('swagger', [SwaggerController::class, 'documentation'])->name('swagger');
-Route::get('swagger-callback', [SwaggerController::class, 'callback'])->name('swagger-callback');
+Route::get('swagger', [SwaggerController::class, 'documentation'])->name('swagger')
+  ->middleware('auth:web');
+Route::get('swagger-callback', [SwaggerController::class, 'callback'])->name('swagger-callback')
+  ->middleware('auth:web');
