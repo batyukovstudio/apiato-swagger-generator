72328c2a7f77163ece64d16ab537c22337244564
Igor Cherepanov <65646152+igor-cherepanov@users.noreply.github.com>
2024-07-23 17:06:31 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,5 @@
 
 use Batyukovstudio\ApiatoSwaggerGenerator\Http\Controllers\SwaggerController;
 
-Route::get('swagger', [SwaggerController::class, 'documentation'])->name('swagger')
-  ->middleware('auth');
-Route::get('swagger-callback', [SwaggerController::class, 'callback'])->name('swagger-callback')
-  ->middleware('auth');
+Route::get('swagger', [SwaggerController::class, 'documentation'])->name('swagger');
+Route::get('swagger-callback', [SwaggerController::class, 'callback'])->name('swagger-callback');
