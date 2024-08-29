4f06882034d50b63fea5b3f001f3ee6379e9e499
leskov-m <leskov760@gmail.com>
2024-08-29 13:35:56 +0500

    feat: auth middleware

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,10 @@
 
 use Batyukovstudio\ApiatoSwaggerGenerator\Http\Controllers\SwaggerController;
 
-Route::get('swagger', [SwaggerController::class, 'documentation'])->name('swagger');
-Route::get('swagger-callback', [SwaggerController::class, 'callback'])->name('swagger-callback');
+Route::get('swagger', [SwaggerController::class, 'documentation'])
+    ->middleware(['auth:web'])
+    ->name('swagger');
+
+Route::get('swagger-callback', [SwaggerController::class, 'callback'])
+    ->middleware(['auth:web'])
+    ->name('swagger-callback');
